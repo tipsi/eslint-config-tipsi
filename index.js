76dcecca6d4e6af1c5d7c81c0cf165002b87bd8b
Anton Kuznetsov <isnifer@gmail.com>
2017-01-19 20:30:55 +0300

    Add rule for comma-dangle

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'exports': 'always-multiline',
+      // Trailing commas in functions are allowed only in ES8
+      // https://github.com/eslint/eslint/issues/7809
+      'functions': 'never',
+    }]
     'global-require': 'off',
     'indent': ['error', 2],
     'no-await-in-loop': 'off',
