3726eb2da5d0de844df94fdc796158faf244d030
Anton Kuznetsov <isnifer@gmail.com>
2017-01-25 10:09:44 +0300

    Config now follow it own rules

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 module.exports = {
-  'parser': 'babel-eslint',
-  'extends': 'airbnb',
-  'env': {
-    'browser': true,
-    'node': true,
-    'es6': true,
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
   },
-  'globals': {
-    '__DEV__': true,
+  globals: {
+    __DEV__: true,
   },
-  'rules': {
+  rules: {
     'react/no-multi-comp': 'off',
     'react/forbid-prop-types': 'off',
     'react/jsx-filename-extension': 'off',
     'react/prefer-stateless-function': 'off',
     'react/jsx-closing-bracket-location': [
       'error', {
-        'selfClosing': 'line-aligned',
-        'nonEmpty': 'after-props'
-      }
+        selfClosing: 'line-aligned',
+        nonEmpty: 'after-props',
+      },
     ],
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
     'comma-dangle': ['error', {
-      'arrays': 'always-multiline',
-      'objects': 'always-multiline',
-      'imports': 'always-multiline',
-      'exports': 'always-multiline',
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
       // Trailing commas in functions are allowed only in ES8
       // https://github.com/eslint/eslint/issues/7809
-      'functions': 'never',
-    }]
+      functions: 'never',
+    }],
     'global-require': 'off',
     'indent': ['error', 2],
     'no-await-in-loop': 'off',
@@ -41,7 +41,7 @@ module.exports = {
     'quote-props': ['error', 'consistent-as-needed'],
     'semi': ['error', 'never'],
   },
-  'plugins': [
+  plugins: [
     'react',
   ],
 }
